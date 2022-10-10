c988beec4ae3142c2f18a42de3c667574f887fd4
Kalireza2 <100313533+Kalireza2@users.noreply.github.com>
2022-10-10 17:30:08 +0330

    Add files via upload

---
+++ app.js
@@ -0,0 +1,172 @@
+// ---hamberger menu---
+const navBtn = document.querySelector('.nav__btn')
+const navMenu = document.querySelector('.nav-menu')
+let navBtnFlag = false
+    navBtn.addEventListener('click', ()=>{
+        if(navBtnFlag){
+            navBtn.classList.add('nav__btn--open')
+            navMenu.style.left=0
+            navBtnFlag=false
+        }else{
+            navBtn.classList.remove('nav__btn--open')
+            navMenu.style.left='-20rem'
+            navBtnFlag=true
+        }
+    })
+// ---hamberger menu---
+
+// ---svg like---
+const svgLike1 = document.querySelector('.likesvg1')
+let flagSvgLike1= false
+        svgLike1.addEventListener('click' , ()=>{
+            if(flagSvgLike1){
+                svgLike1.style.fill="#e76f51"
+                flagSvgLike1= false
+            }else{
+                svgLike1.style.fill="#fff"
+                flagSvgLike1= true
+            }
+        })
+
+const svgLike2 = document.querySelector('.likesvg2')
+let flagSvgLike2= false
+        svgLike2.addEventListener('click' , ()=>{
+            if(flagSvgLike2){
+                svgLike2.style.fill="#e76f51"
+                flagSvgLike2= false
+            }else{
+                svgLike2.style.fill="#fff"
+                flagSvgLike2= true
+            }
+        })
+
+const svgLike3 = document.querySelector('.likesvg3')
+let flagSvgLike3= false
+        svgLike3.addEventListener('click' , ()=>{
+            if(flagSvgLike3){
+                svgLike3.style.fill="#e76f51"
+                flagSvgLike3= false
+            }else{
+                svgLike3.style.fill="#fff"
+                flagSvgLike3= true
+            }
+        })
+
+const svgLike4 = document.querySelector('.likesvg4')
+let flagSvgLike4= false
+        svgLike4.addEventListener('click' , ()=>{
+            if(flagSvgLike4){
+                svgLike4.style.fill="#e76f51"
+                flagSvgLike4= false
+            }else{
+                svgLike4.style.fill="#fff"
+                flagSvgLike4= true
+            }
+        })
+
+
+const svgLike5 = document.querySelector('.likesvg5')
+let flagSvgLike5= false
+        svgLike5.addEventListener('click' , ()=>{
+            if(flagSvgLike5){
+                svgLike5.style.fill="#e76f51"
+                flagSvgLike5= false
+            }else{
+                svgLike5.style.fill="#fff"
+                flagSvgLike5= true
+            }
+        })
+
+
+const svgLike6 = document.querySelector('.likesvg6')
+let flagSvgLike6= false
+        svgLike6.addEventListener('click' , ()=>{
+            if(flagSvgLike6){
+                svgLike6.style.fill="#e76f51"
+                flagSvgLike6= false
+            }else{
+                svgLike6.style.fill="#fff"
+                flagSvgLike6= true
+            }
+        })
+
+
+const svgLike7 = document.querySelector('.likesvg7')
+let flagSvgLike7= false
+        svgLike7.addEventListener('click' , ()=>{
+            if(flagSvgLike7){
+                svgLike7.style.fill="#e76f51"
+                flagSvgLike7= false
+            }else{
+                svgLike7.style.fill="#fff"
+                flagSvgLike7= true
+            }
+        })
+
+
+const svgLike8 = document.querySelector('.likesvg8')
+let flagSvgLike8= false
+        svgLike8.addEventListener('click' , ()=>{
+            if(flagSvgLike8){
+                svgLike8.style.fill="#e76f51"
+                flagSvgLike8= false
+            }else{
+                svgLike8.style.fill="#fff"
+                flagSvgLike8= true
+            }
+        })
+
+
+const svgLike9 = document.querySelector('.likesvg9')
+let flagSvgLike9= false
+        svgLike9.addEventListener('click' , ()=>{
+            if(flagSvgLike9){
+                svgLike9.style.fill="#e76f51"
+                flagSvgLike9= false
+            }else{
+                svgLike9.style.fill="#fff"
+                flagSvgLike9= true
+            }
+        })
+
+
+const svgLike10 = document.querySelector('.likesvg10')
+let flagSvgLike10= false
+        svgLike10.addEventListener('click' , ()=>{
+            if(flagSvgLike10){
+                svgLike10.style.fill="#e76f51"
+                flagSvgLike10= false
+            }else{
+                svgLike10.style.fill="#fff"
+                flagSvgLike10= true
+            }
+        })
+
+
+const svgLike11 = document.querySelector('.likesvg11')
+let flagSvgLike11= false
+        svgLike11.addEventListener('click' , ()=>{
+            if(flagSvgLike11){
+                svgLike11.style.fill="#e76f51"
+                flagSvgLike11= false
+            }else{
+                svgLike11.style.fill="#fff"
+                flagSvgLike11= true
+            }
+        })
+
+
+const svgLike12 = document.querySelector('.likesvg12')
+let flagSvgLike12= false
+        svgLike12.addEventListener('click' , ()=>{
+            if(flagSvgLike12){
+                svgLike12.style.fill="#e76f51"
+                flagSvgLike12= false
+            }else{
+                svgLike12.style.fill="#fff"
+                flagSvgLike12= true
+            }
+        })
+
+
+// ---svg like---
---
+++ script.js
@@ -0,0 +1,303 @@
+// ---svg like---
+//     const svgLike1 = document.querySelector('.likesvg1')
+//     let flagSvgLike1= false
+//             svgLike1.addEventListener('click' , ()=>{
+//                 if(flagSvgLike1){
+//                     svgLike1.style.fill="#e76f51"
+//                     flagSvgLike1= false
+//                 }else{
+//                     svgLike1.style.fill="#fff"
+//                     flagSvgLike1= true
+//                 }
+//             })
+
+// const svgLike2 = document.querySelector('.likesvg2')
+// let flagSvgLike2= false
+//         svgLike2.addEventListener('click' , ()=>{
+//             if(flagSvgLike2){
+//                 svgLike2.style.fill="#e76f51"
+//                 flagSvgLike2= false
+//             }else{
+//                 svgLike2.style.fill="#fff"
+//                 flagSvgLike2= true
+//             }
+//         })
+
+// const svgLike3 = document.querySelector('.likesvg3')
+// let flagSvgLike3= false
+//         svgLike3.addEventListener('click' , ()=>{
+//             if(flagSvgLike3){
+//                 svgLike3.style.fill="#e76f51"
+//                 flagSvgLike3= false
+//             }else{
+//                 svgLike3.style.fill="#fff"
+//                 flagSvgLike3= true
+//             }
+//         })
+
+// const svgLike4 = document.querySelector('.likesvg4')
+// let flagSvgLike4= false
+//         svgLike4.addEventListener('click' , ()=>{
+//             if(flagSvgLike4){
+//                 svgLike4.style.fill="#e76f51"
+//                 flagSvgLike4= false
+//             }else{
+//                 svgLike4.style.fill="#fff"
+//                 flagSvgLike4= true
+//             }
+//         })
+
+
+// const svgLike5 = document.querySelector('.likesvg5')
+// let flagSvgLike5= false
+//         svgLike5.addEventListener('click' , ()=>{
+//             if(flagSvgLike5){
+//                 svgLike5.style.fill="#e76f51"
+//                 flagSvgLike5= false
+//             }else{
+//                 svgLike5.style.fill="#fff"
+//                 flagSvgLike5= true
+//             }
+//         })
+
+
+// const svgLike6 = document.querySelector('.likesvg6')
+// let flagSvgLike6= false
+//         svgLike6.addEventListener('click' , ()=>{
+//             if(flagSvgLike6){
+//                 svgLike6.style.fill="#e76f51"
+//                 flagSvgLike6= false
+//             }else{
+//                 svgLike6.style.fill="#fff"
+//                 flagSvgLike6= true
+//             }
+//         })
+
+
+// const svgLike7 = document.querySelector('.likesvg7')
+// let flagSvgLike7= false
+//         svgLike7.addEventListener('click' , ()=>{
+//             if(flagSvgLike7){
+//                 svgLike7.style.fill="#e76f51"
+//                 flagSvgLike7= false
+//             }else{
+//                 svgLike7.style.fill="#fff"
+//                 flagSvgLike7= true
+//             }
+//         })
+
+
+// const svgLike8 = document.querySelector('.likesvg8')
+// let flagSvgLike8= false
+//         svgLike8.addEventListener('click' , ()=>{
+//             if(flagSvgLike8){
+//                 svgLike8.style.fill="#e76f51"
+//                 flagSvgLike8= false
+//             }else{
+//                 svgLike8.style.fill="#fff"
+//                 flagSvgLike8= true
+//             }
+//         })
+
+
+// const svgLike9 = document.querySelector('.likesvg9')
+// let flagSvgLike9= false
+//         svgLike9.addEventListener('click' , ()=>{
+//             if(flagSvgLike9){
+//                 svgLike9.style.fill="#e76f51"
+//                 flagSvgLike9= false
+//             }else{
+//                 svgLike9.style.fill="#fff"
+//                 flagSvgLike9= true
+//             }
+//         })
+
+
+// const svgLike10 = document.querySelector('.likesvg10')
+// let flagSvgLike10= false
+//         svgLike10.addEventListener('click' , ()=>{
+//             if(flagSvgLike10){
+//                 svgLike10.style.fill="#e76f51"
+//                 flagSvgLike10= false
+//             }else{
+//                 svgLike10.style.fill="#fff"
+//                 flagSvgLike10= true
+//             }
+//         })
+
+
+// const svgLike11 = document.querySelector('.likesvg11')
+// let flagSvgLike11= false
+//         svgLike11.addEventListener('click' , ()=>{
+//             if(flagSvgLike11){
+//                 svgLike11.style.fill="#e76f51"
+//                 flagSvgLike11= false
+//             }else{
+//                 svgLike11.style.fill="#fff"
+//                 flagSvgLike11= true
+//             }
+//         })
+
+
+// const svgLike12 = document.querySelector('.likesvg12')
+// let flagSvgLike12= false
+//         svgLike12.addEventListener('click' , ()=>{
+//             if(flagSvgLike12){
+//                 svgLike12.style.fill="#e76f51"
+//                 flagSvgLike12= false
+//             }else{
+//                 svgLike12.style.fill="#fff"
+//                 flagSvgLike12= true
+//             }
+//         })
+
+
+// ---svg like---
+
+// === start script === //
+
+// === DB Products === //
+const products = [
+    {id: 1 , image: 'image1.jpg' , price: 36 , name: 'کابین کوچک در کوه' , },
+    {id: 2 , image: 'image2.jpg' , price: 36 , name: 'کابین کوچک سنجاقک' , },
+    {id: 3 , image: 'image3.jpg' , price: 36 , name: 'مرتع تریک کریک' , },
+    {id: 4 , image: 'image4.jpg' , price: 36 , name: 'کابین ارمیتاژ' , },
+    {id: 5 , image: 'image5.jpg' , price: 36 , name: 'کابین ارواح خویشاوند' , },
+    {id: 6 , image: 'image6.jpg' , price: 36 , name: 'کابین اجلاس سران' , },
+    {id: 7 , image: 'image7.jpg' , price: 36 , name: 'کابین لوکس کوچک ساحلی' , },
+    {id: 8 , image: 'image8.jpg' , price: 36 , name: 'نینووان در ساحل' , },
+    {id: 9 , image: 'image9.jpg' , price: 36 , name: 'چادر مرتفع جزیره مانع' , },
+    {id: 10 , image: 'image10.jpg' , price: 36 , name: 'دریاچه ارومیه' , },
+    {id: 11 , image: 'image11.jpg' , price: 36 , name: 'کمپ شبانه کوه های آلپ' , },
+    {id: 12 , image: 'image12.jpg' , price: 36 , name: 'رودخانه مازندران' , },
+
+]
+
+// --- start dinamik wrapper products --- //
+const renderProducts = ()=>{
+    const productDiv = document.querySelector('.Destination__places')
+    productDiv.innerHTML = ''
+    
+    products.forEach( (item , index)=>{
+        productDiv.innerHTML +=`
+        <section class="place">
+        <div class="place__baner">
+        <img class="place__baner__img" src=${item.image} alt="">
+        <div class="place__baner__like">
+        <svg  class="svg-like likesvg1" xmlns="http://www.w3.org/2000/svg" width="24.037" height="24.037" viewBox="0 0 24.037 24.037" fill="#fff" stroke="#e76f51" stroke-width="1.8" >
+        <path id="Path_955" data-name="Path 955" d="M15.822,8.979v.029H4V-2.1A5.915,5.915,0,0,1,9.912-7.841a5.913,5.913,0,0,1,5.849,5.049,5.912,5.912,0,0,1,.061,11.772Z" transform="translate(2.716 20.496) rotate(-45)" />
+        </svg>                              
+        </div>
+        <div class="place__baner__price">
+        <p class="price">
+        $${item.price}/شبانه
+        </p>
+        </div>
+        </div>
+        <h3 class="place__title">${item.name}</h3>
+               <p class="place__caption">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم</p>
+               <button class="btn place__link" onclick="addToBascket(${index})">اضافه کردن به سبد خرید</button>
+               </section>
+               `
+            } )
+        }
+        
+
+        let Bascket ={
+            item: [],
+            AllPrice: 0,
+        }
+
+
+        const renderBascket =()=>{
+            const bascketDiv = document.querySelector('.allBasketPrice')
+                    bascketDiv.innerHTML=''
+
+                const totalProductPrice = document.querySelector('.total-products-price')
+                let totalPrice = 0
+
+                if(Bascket.item.length===0){
+                    bascketDiv.innerHTML="سبد خرید شما خالی است -_- "
+                }
+
+                Bascket.item.forEach( (item)=>{
+                    totalPrice += item.AllPrice
+                    bascketDiv.innerHTML+=`
+                    <div class="wrapper-bascket">
+                    <div class="btn__delete">
+                        <button class="btn-delete" onclick="removeProductBascket('${item.name}')" >حذف</button>
+                    </div>
+                    <div class="qty">
+                        <p class="qty-number">${item.qty}</p>
+                    </div>
+                    <div class="price-procducts">
+                        <p class="product__price">$${item.price}</p>
+                    </div>
+                    <div class="productn-n">
+                        <p class="productn-n__name">${item.name}</p>
+                    </div>
+                </div>
+                    `
+                } )
+
+                totalProductPrice.innerHTML= `مجموع قیمت: $${totalPrice}  <a href="#" class="payment">پرداخت</a>`
+        }
+
+
+        const addToBascket = (productIndex)=>{
+            const product = products[productIndex]
+            let flagProduct = false
+
+            let newproductItems = Bascket.item.reduce((stats , item)=>{
+                if(item.name===product.name){
+                    flagProduct=true
+
+                    const newItem={
+                        ...item,
+                        qty: item.qty+1,
+                        AllPrice: (item.qty+1)*item.price
+                    }
+                    return [...stats , newItem]
+                }
+                return [...stats , item]
+            },[])
+
+            if(!flagProduct){
+                newproductItems.push({
+                    ...product,
+                    qty: 1,
+                    AllPrice: product.price
+                })
+            }
+            Bascket = {
+                ...Bascket,
+                item: newproductItems
+            }
+            renderBascket()
+        }
+
+        const removeProductBascket = (productsName)=>{
+            let removeProduct = Bascket.item.reduce((stats , item)=>{
+                if(item.name===productsName){
+                    const newRemove ={
+                        ...item,
+                        qty: item.qty-1,
+                        AllPrice: (item.qty-1) * item.price
+                    }
+                    if(newRemove.qty > 0){
+                        return [...stats , newRemove]
+                    }else{
+                        return stats
+                    }
+                }
+                return [...stats , item]
+            } ,[])
+            Bascket = {
+                ...Bascket,
+                item: removeProduct
+            }
+            renderBascket()
+        }
+
+        renderProducts()
+        renderBascket()
